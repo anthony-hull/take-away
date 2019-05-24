179feb0ec80383d47e6f45858f6f0e1d73469306
Anthony Hull <m@anthonyhull.me>
2019-05-24 14:55:56 +0100

    implemented FoodMenu.price

--- lib/FoodItem.rb
+++ lib/FoodItem.rb
@@ -1,3 +1,3 @@
 class FoodItem
-  attr_accessor :name
+  attr_accessor :name, :price
 end
--- spec/FoodItem_spec.rb
+++ spec/FoodItem_spec.rb
@@ -2,7 +2,11 @@ require 'FoodItem'
 describe FoodItem do
   subject { FoodItem.new }
   it 'has name set to Pizza' do
-      expect(subject).to receive(:name=).with('Pizza')
-      subject.name = 'Pizza'
+    expect(subject).to receive(:name=).with('Pizza')
+    subject.name = 'Pizza'
+  end
+  it 'has a price set to 300 pence' do
+    expect(subject).to receive(:price=).with(300)
+    subject.price = 300
   end
 end
